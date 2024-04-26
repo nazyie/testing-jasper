9071bd959aa0a6d0e297828ec977e840f94a82f9
Engku Nazri <nazyie.d@gmail.com>
2024-04-26 08:14:05 +0800

    notes

--- src/main/java/com/example/demojasper/controller/TestController.java
+++ src/main/java/com/example/demojasper/controller/TestController.java
@@ -56,4 +56,70 @@ public class TestController {
     }
 
 
+    import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRDocxExporter;
+import net.sf.jasperreports.engine.export.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleWriterExporterOutput;
+
+// ...
+
+private void generateReport() {
+    try {
+        // Compile the Jasper report from .jrxml to .japser
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportPath);
+
+        // Parameters for report
+        Map<String, Object> parameters = new HashMap<String, Object>();
+
+        // DataSource
+        // This is simple example, no database.
+        // then using empty datasource.
+        Collection<Map<String, ?>> dataSource = Collections.singletonList(Collections.singletonMap("data", "test data"));
+
+        JRDataSource jrDataSource = new JRBeanCollectionDataSource(dataSource);
+
+        JasperPrint print = JasperFillManager.fillReport(jasperReport, parameters, jrDataSource);
+
+        // Make sure the output directory exists.
+        File outDir = new File(targetPath);
+        outDir.mkdirs();
+
+        // Export to PDF.
+        JasperExportManager.exportReportToPdfFile(print, targetPath + "simple_report.pdf");
+
+        // Export to HTML.
+        JasperExportManager.exportReportToHtmlFile(print, targetPath + "simple_report.html");
+
+        // Export to XML.
+        JasperExportManager.exportReportToXmlFile(print, targetPath + "simple_report.xml", false);
+
+        // Export to XLS.
+        JRXlsxExporter xlsExporter = new JRXlsxExporter();
+        xlsExporter.setExporterInput(new SimpleExporterInput(print));
+        xlsExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(targetPath + "simple_report.xlsx"));
+        xlsExporter.exportReport();
+
+        // Export to CSV.
+        JRCsvExporter csvExporter = new JRCsvExporter();
+        csvExporter.setExporterInput(new SimpleExporterInput(print));
+        csvExporter.setExporterOutput(new SimpleWriterExporterOutput(targetPath + "simple_report.csv"));
+        csvExporter.exportReport();
+
+        // Export to DOCX.
+        JRDocxExporter docxExporter = new JRDocxExporter();
+        docxExporter.setExporterInput(new SimpleExporterInput(print));
+        docxExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(targetPath + "simple_report.docx"));
+        docxExporter.exportReport();
+
+        System.out.println("Done!");
+
+    } catch (JRException e) {
+        e.printStackTrace();
+    }
+}
+
 }
